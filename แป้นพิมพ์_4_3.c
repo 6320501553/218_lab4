db7472c2f03986dfd59e3a1026cf5f4381d91d79
6320501553 <78528282+6320501553@users.noreply.github.com>
2021-03-04 15:37:10 +0700

    edit

--- แป้นพิมพ์_4_3.c
+++ แป้นพิมพ์_4_3.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
-void main()
+#include<string.h>
+int main()
 {
     char cha[201];
     int i;
     scanf("%s",cha);
     for(i=0; i<strlen(cha); i++)
     {
-        if(i==strlen(cha))
-            break;
-        else if(cha[i]!=cha[i+1])
+        if(cha[i]!=cha[i+1])
         {
             printf("%c",cha[i]);
         }
     }
+    return 0;
 }
