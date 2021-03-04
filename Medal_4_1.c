f41cbb26f5ecfb38c8aa7e39b7503c9e04f86b75
6320501553 <78528282+6320501553@users.noreply.github.com>
2021-03-04 17:22:40 +0700

    edit

--- Medal_4_1.c
+++ Medal_4_1.c
@@ -1,14 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int n,k;
+    int n;
     scanf("%d",&n);
-    k=n/3;
-    int a[n][2],i,j,max[n],sp[k];
+    int a[n][2],i,j;
     for(i=0; i<n; i++)
     {
-        max[i]=0;
-        sp[i]=0;
         for(j=0; j<3; j++)
         {
             scanf("%d",&a[i][j]);
@@ -16,16 +13,30 @@ int main()
                 a[i][j]=a[i][j]*4;
             else if(j==1)
                 a[i][j]=a[i][j]*2;
-            else
+            else if(j==2)
                 a[i][j]=a[i][j]*1;
-            max[i]+=a[i][j];
-            if(i<3)
-            {
-                sp[i]+=max[i];
-            }
         }
-        printf("SP %d\n",sp[i]);
     }
+    int locamax=0,locamin=0,min=2000,max=0,sum=0;
+    for(i=0;i<n-2;i++)
+    {
+        for(j=i; j<i+3; j++)
+        {
+            sum=sum+a[j][0]+a[j][1]+a[j][2];
+        }
+        if(max<sum)
+        {
+            max=sum;
+            locamax=i;
+        }
+        if(min>sum)
+        {
+            min=sum;
+            locamin=i;
+        }
+        sum=0;
+    }
+    printf("%d %d\n",locamax+1,locamin+1);
 
     return 0;
 }
