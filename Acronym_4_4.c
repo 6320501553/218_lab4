ffca6edd76109a1d51810df74d5e7f42935160a7
6320501553 <78528282+6320501553@users.noreply.github.com>
2021-03-04 16:15:31 +0700

    add edit

--- Acronym_4_4.c
+++ Acronym_4_4.c
@@ -2,15 +2,17 @@
 #include<string.h>
 int main()
 {
-    char cha,n[]=" ";
+    char cha[1001],n=" ";
     scanf("%s",cha);
     int i;
     for(i=0; i<strlen(cha); i++)
     {
-        if(cha[i] == n)
+        if(i==0)
         {
-            printf("%c",cha[i+1]);
+            printf("%c",cha[i]);
         }
+        else if(cha[i]==" ")
+            printf("%c",cha[i+1]);
     }
     return 0;
 }
